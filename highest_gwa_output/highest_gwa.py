89830f71d33c0be7c84b933649e7982518a92ec7
Clark Domanais <clarkdomanais@gmail.com>
2026-04-29 02:44:23 +0800

    added a function where it prints the student with the highest gwa and and if there is no student found, it prints out the no students found string

--- highest_gwa_output/highest_gwa.py
+++ highest_gwa_output/highest_gwa.py
@@ -14,3 +14,7 @@ if __name__ == "__main__":
     finder = GWAFinder('students.txt')
     finder.read_students()
     highest_student = finder.find_highest()
+    if highest_student:
+        print(f"The student with the highest GWA is {highest_student.name} with a GWA of {highest_student.gwa}")
+    else:
+        print("No students found.")
