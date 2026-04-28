f54d2ec0c57a70aef95a52904f4ff9ebe90692c3
Clark Domanais <clarkdomanais@gmail.com>
2026-04-28 23:05:55 +0800

    handling error: using try and except function

--- input_file.py
+++ input_file.py
@@ -1,6 +1,12 @@
 def main():
     input_file = "numbers.txt"
 
+    try:
+        with open(input_file, "r") as infile:
+            text = infile.read()
+    except FileNotFoundError:
+        print(f"Input file '{input_file}' not found.")
+        return
 
 
 if __name__ == "__main__":
