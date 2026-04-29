2ecf76bb8e0d8bc0000cabcecf54b3277ebadb2c
Clark Domanais <clarkdomanais@gmail.com>
2026-04-29 22:09:07 +0800

    lets the used enter multiple lines of text interactively and saves them in a text file until they answer an or any non-y response.

--- my_life/my_life.py
+++ my_life/my_life.py
@@ -2,5 +2,13 @@ class MyLife:
     def __init__(self, file_name):
         self.file_name = file_name
 
+    def write_lines(self):
+        with open(self.file_name, "w") as file:
+            while True:
+                line = input("Enter line: ")
+                file.write(line + "\n")
 
+                more = input("Are there more lines y/n ").strip().lower()
+                if more != 'y':
+                    break
 
